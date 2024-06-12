9e3d46762c026e3d4945cd4797932c7978999076
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-06-12 17:57:34 -0300

    :recycle:refactor: refatora a componentização das Social Medias no footer.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,24 @@
 import Credits from "./Credits";
 import SocialMedias from "./SocialMedias";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const socialMedias = [
+    {
+        icon: <GitHubIcon />,
+        link: "https://github.com/allessandrogomes"
+    },
+    {
+        icon: <LinkedInIcon />,
+        link: "https://linkedin.com/in/allessandrogomes"
+    }
+]
 
 export default function Footer() {
     return (
         <div className="text-center h-28 bg-teal-400 flex items-center justify-center font-archivo flex-col">
             <Credits />
-            <SocialMedias />
+            <SocialMedias socialMedias={socialMedias}/>
         </div>
     )
 }
