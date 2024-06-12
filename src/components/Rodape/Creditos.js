30d2bfcdae8e5f225e87b91defe4b1b19d98be1a
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-06-12 18:12:09 -0300

    :wastebasket:remove: remove arquivos do footer anteriores, que foram refatorados.

--- src/components/Rodape/Creditos.js
+++
@@ -1,9 +0,0 @@
-function Creditos() {
-    return (
-        <h3 className="mb-2">Desenvolvido por <span className="font-bold"><a target="_blank"
-            href="https://allessandrogomes.github.io/portfolio/">Alessandro Gomes</a></span> &copy; 2023
-        </h3>
-    )
-}
-
-export default Creditos
