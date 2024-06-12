bf08e95193563da596187fc5252df071908a3cbb
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-06-12 18:03:38 -0300

    :broom:cleanup: Limpa estilizações desnecessárias no Banner.

--- src/components/Main/Banner/index.jsx
+++ src/components/Main/Banner/index.jsx
@@ -1,8 +1,8 @@
 export default function Banner() {
     return (
-        <section className="sectionChamada h-400 bg-banner bg-no-repeat bg-cover bg-teal-400 flex flex-col items-center justify-center mt-[120px]">
-            <h1 className="sectionChamadaTitulo text-font-56px font-big-shoulders-display font-black text-white">Carros é na CarsOn</h1>
-            <h2 className="sectionChamadaSubtitulo font-archivo text-white text-2xl font-medium">Compra e venda de veículos</h2>
+        <section className="h-400 bg-banner bg-no-repeat bg-cover bg-teal-400 flex flex-col items-center justify-center mt-[120px]">
+            <h1 className="text-6xl font-big-shoulders-display font-black text-white">Carros é na CarsOn</h1>
+            <h2 className="font-archivo text-white text-2xl font-medium">Compra e venda de veículos</h2>
         </section>
     )
 }
