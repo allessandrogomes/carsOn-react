c2b3f33a92e4d89bf4f2c3a0947bcbf57143c379
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-06-19 19:43:12 -0300

    :recycle:refactor: refatora a página Home.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,36 +3,18 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import Header from "../../components/Header"
 import Banner from "../../components/Main/Banner"
 import SessionFilters from '../../components/Main/SessionFilters'
 import SessionCars from '../../components/Main/SessionCars'
-import Footer from '../../components/Footer'
-import { useState } from 'react'
-import ModalRegister from '../../components/Main/ModalRegister'
 
 export default function Home() {
-
-    const [openModalRegister, setOpenModalRegister] = useState(false)
-
     return (
         <div className="flex flex-col">
-            <header>
-                <Header openModalRegister={() => setOpenModalRegister(true)}/>
-            </header>
-
-            <main>
-                {openModalRegister && <ModalRegister onClose={() => setOpenModalRegister(false)}/>}
-                <Banner />
-                <div className="flex">
-                    <SessionFilters />
-                    <SessionCars />
-                </div>
-            </main>
-
-            <footer>
-                <Footer />
-            </footer>
+            <Banner />
+            <div className="flex">
+                <SessionFilters />
+                <SessionCars />
+            </div>
         </div>
     )
 }
