f8bab45e5b508d922db289a4c7206573ef629114
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-06-19 19:44:21 -0300

    :recycle:refactor: refatora as Rotas, adicionando a DefaultPage para reaproveitamento de elementos em outras páginas.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Advertisement from "./pages/Advertisement";
+import DefaultPage from "./components/DefaultPage";
 
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />}/>
+                <Route path="/" element={<DefaultPage />}>
+                    <Route index element={<Home />} />
+                    <Route path="/advertisement" element={<Advertisement />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
