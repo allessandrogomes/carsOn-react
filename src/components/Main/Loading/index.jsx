64e7ec74f2bd4b9d382606b010e4dea11091aee3
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-06-19 19:47:00 -0300

    :sparkles:feat: cria o componente Loading para informar ao usuário quando os dados estão sendo carregados.

---
+++ src/components/Main/Loading/index.jsx
@@ -0,0 +1,11 @@
+import { Box, CircularProgress, Typography } from "@mui/material";
+
+
+export default function Loading() {
+    return (
+        <Box sx={{ display: 'flex', alignSelf: 'center', alignItems: 'center', mt: '100px', gap: '15px' }}>
+            <Typography fontWeight="bold">Carregando</Typography>
+            <CircularProgress />
+        </Box>
+    )
+}
