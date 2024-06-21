009178d3b80821cfba00b6e9fed7f5542ae601b9
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-06-21 19:00:03 -0300

    :wastebasket:remove: remove os reducers ClickedAd e SearchAd.

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import usersSlice from './reducers/users'
 import advertisementsSlice from './reducers/advertisements'
-import clickeAdSlice from './reducers/clickedAd'
-import searchValueSlice from './reducers/searchValue'
-
 
 const store = configureStore({
     reducer: {
         users: usersSlice,
-        advertisements: advertisementsSlice,
-        clickedAd: clickeAdSlice,
-        searchValue: searchValueSlice
+        advertisements: advertisementsSlice
     }
 })
 
